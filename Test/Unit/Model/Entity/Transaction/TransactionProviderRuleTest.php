cd2aec791e68ec3ecbfec0c2d33794d2ed3c1ef9
Laurynas <l.griciunas@gmail.com>
2019-12-24 15:53:18 +0200

    add unit test for transaction provider rules

---
+++ Test/Unit/Model/Entity/Transaction/TransactionProviderRuleTest.php
@@ -0,0 +1,100 @@
+<?php
+namespace Test\Unit\Model\Entity\Transaction;
+
+use Kernolab\Model\Entity\Transaction\TransactionProviderRule;
+use PHPUnit\Framework\TestCase;
+
+class TransactionProviderRuleTest extends TestCase
+{
+    private $providers;
+    
+    private $transactionProvider;
+    
+    protected function setUp(): void
+    {
+        $this->providers           = [
+            "EUR"     => [
+                "name"  => "Megacash",
+                "rules" => [
+                    "field" => "transaction_details",
+                    "rule"  => "length",
+                    "value" => "20",
+                ],
+            ],
+            "Default" => [
+                "name"  => "Megacash",
+                "rules" => [
+                    "field" => "transaction_details",
+                    "rule"  => "random_int",
+                    "value" => "50",
+                ],
+            ],
+        ];
+        $this->transactionProvider = new TransactionProviderRule();
+    }
+    
+    protected function tearDown(): void
+    {
+        $this->transactionProvider = null;
+        $this->providers           = null;
+    }
+    
+    /**
+     * @dataProvider applyProviderRulesMegacashProvider
+     *
+     * @param $params
+     * @param $expected
+     */
+    public function testApplyProviderRulesMegacash($params, $expected)
+    {
+        $actual = $this->transactionProvider->applyProviderRules($params);
+        $this->assertEquals($expected, $actual);
+    }
+    
+    public function applyProviderRulesMegacashProvider()
+    {
+        return [
+            [
+                [
+                    "transaction_details"  => "20 characters here: this should not be seen",
+                    "transaction_currency" => "EUR",
+                ],
+                [
+                    "transaction_details"  => "20 characters here: ",
+                    "transaction_currency" => "EUR",
+                    "transaction_provider" => "Megacash"
+                ]
+            ],
+        ];
+    }
+    
+    /**
+     * @dataProvider applyProviderRulesSupermoneyProvider
+     *
+     * @param $params
+     * @param $expected
+     */
+    public function testApplyProviderRulesSupermoney($params, $expected)
+    {
+        $actual = $this->transactionProvider->applyProviderRules($params);
+        $this->assertRegExp("/Details \d+/", $actual["transaction_details"]);
+        $this->assertEquals($expected["transaction_provider"], $actual["transaction_provider"]);
+    }
+    
+    public function applyProviderRulesSupermoneyProvider()
+    {
+        return [
+            [
+                [
+                    "transaction_details"  => "Details ",
+                    "transaction_currency" => "USD",
+                ],
+                [
+                    "transaction_details"  => "Details ",
+                    "transaction_currency" => "USD",
+                    "transaction_provider" => "Supermoney"
+                ]
+            ],
+        ];
+    }
+}
